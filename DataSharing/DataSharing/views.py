22d53185f3ec1e3475980a1e3951394a13666453
Eviatar Zilberman <esztbk@gmail.com>
2024-02-17 21:20:37 +0200

    Start of User class

--- DataSharing/DataSharing/views.py
+++ DataSharing/DataSharing/views.py
@@ -12,7 +12,7 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='Home Page',
+        title='Home Page', # Argument that sent to the page title.
         year=datetime.now().year,
     )
 
---
+++ DataSharing/Models/User.py
@@ -0,0 +1,11 @@
+from PyMongoDb import IPyMongoDb
+
+class User(IPyMongoDb, object):
+    m_id = None
+    m_first_name = None
+    m_last_name = None
+    m_email = None
+    m_password = None
+    
+
+
